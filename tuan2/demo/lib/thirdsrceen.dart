4641b6468b8d3d67ed533b9dfe7040b1d3057457
anhduc317 <58928601+anhduc317@users.noreply.github.com>
2021-06-17 15:25:06 +0700

    Tuan 2

---
+++ tuan2/demo/lib/thirdsrceen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ThirdScreen'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+
+      ),
+    );
+  }
+}
