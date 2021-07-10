098ecca03367dda8be59ee5eb9201e8431709703
anhduc317 <58928601+anhduc317@users.noreply.github.com>
2021-07-10 20:40:23 +0700

    Demo4-5

---
+++ tuan4-5/Demo4_5/lib/model/homelist.dart
@@ -0,0 +1,25 @@
+import 'package:best_flutter_ui_templates/design_course/home_design_course.dart';
+import 'package:best_flutter_ui_templates/hotel_booking/hotel_home_screen.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+
+    HomeList(
+      imagePath: 'assets/hotel/hotel_booking.png',
+      navigateScreen: HotelHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/design_course.png',
+      navigateScreen: DesignCourseHomeScreen(),
+    ),
+  ];
+}
---
+++ tuan4-5/Demo4_5/lib/src/resoureces/secondscreen.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import '../../repositories/Services.dart';
+import '../../models/Users.dart';
+import 'package:http/http.dart' as http;
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen();
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  late Users _users;
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController fullnameController = TextEditingController();
+
+  late List<Users> _lsusers;
+  late bool _loading;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    aBC();
+    super.initState();
+  }
+
+  void aBC() {
+    _loading = true;
+    Services.getUsers().then((users) {
+      _lsusers = users;
+      _loading = false;
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SecondScreen'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Enter Username'),
+                controller: usernameController,
+              ),
+              SizedBox(height: 5),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Enter Fullname'),
+                controller: fullnameController,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    String username = usernameController.text;
+                    String fullname = fullnameController.text;
+                    Users data = await postData(username, fullname);
+                    setState(() {
+                      _users = data;
+                    });
+                  },
+                  child: Text("Submit")),
+              Container(
+                height: 500,
+                child: ListView.builder(
+                    itemCount: _lsusers?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      Users user = _lsusers[index];
+                      return ListTile(
+                        onTap: () => goPage(_lsusers[index]),
+                        title: Text(user.userName),
+                        subtitle: Text(user.fullName),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void goPage(Users data) {}
+
+  // Post DATA
+  Future<Users> postData(String username, String fullname) async {
+    var response = await http.post(
+        Uri.parse('http://demo.vietinfo.tech:8091/api/user/Demo_Insert'),
+        body: jsonEncode({'username': username, 'fullname': fullname}),
+        headers: {'Content-Type': 'application/json'});
+    var users = response.body;
+    print(users);
+    if (response.statusCode == 200) {
+      String responseString = response.body;
+      aBC();
+    } else {
+      throw Exception('Failed to submit user.');
+    }
+    return postData(username, fullname);
+  }
+}
+
+class Object {
+  int id = 0;
+}
+
